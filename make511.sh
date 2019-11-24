d7adfe1d87e922de2b773ad80ef655a5d1e64cb3
80036nd <40246027+80036nd@users.noreply.github.com>
2019-11-24 13:00:19 -0500

    Update make511.sh

--- make511.sh
+++ make511.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 cd ~/Documents/Pluvia-1.5
 chmod +x ./make_ipsw.sh
-chmod +x /tools/xpwntool
+chmod +x ./tools/xpwntool
 ./make_ipsw.sh ~/Downloads/iPhone3\,1_5.1.1_9B206_Restore.ipsw
 killall Terminal
